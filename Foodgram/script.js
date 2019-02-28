13786670e049e81526e13d0acd72c02461756e5f
chastity duhaney <chastity_duhaney82@gmail.com>
2019-02-28 11:59:54 -0500

    creating parent element clickeach picture

--- Foodgram/script.js
+++ Foodgram/script.js
@@ -7,10 +7,14 @@ const targetElements = document.querySelectorAll("[data-target]");
 // We see an enlarged version of image.
 // inside that .picture-frame
 
-function respondToClick(){
+function respondToClick(event) {
     console.log('hello');
     console.log('how are you?');
     console.log('I am great I am eating seafood');
+
+    console.log(event.target.parentElement);
+    // add the "big" class to the thing that got clicked
+    event.target.parentElement.classList.add("big");
 }
 function attachClickHandler(oneElement) {
    // This is how we handle one element.
