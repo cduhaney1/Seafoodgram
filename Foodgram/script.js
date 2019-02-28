0f1522e7e549fd72538728658070df6acf59545c
chastity duhaney <chastity_duhaney82@gmail.com>
2019-02-28 11:26:27 -0500

    adds click handler function

---
+++ Foodgram/script.js
@@ -0,0 +1,15 @@
+console.log("is this thing on? hello?")
+
+// When we click any .picture-frame
+// A const is a "constant variable "
+// which means it can not be reassigned
+const targetElements = document.querySelectorAll("[data-target]");
+// We see an enlarged version of image.
+// inside that .picture-frame
+
+function respondToClick(){
+    console.log('hello');
+    console.log('how are you?');
+    console.log('I am great I am eating seafood');
+}
+targetElements[0].addEventListener("click", respondToClick);
